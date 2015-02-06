a26b0dc46d5d625833c6448ef85db315f5836af2
BaillieC <gfb11176@uni.strath.ac.uk>
2015-02-06 14:08:53 +0000

    Added a main package and class so it is clear where the program is run from, rather than having to use MainMenu.jav from the view package

---
+++ src/Main/Main.java
@@ -0,0 +1,36 @@
+package Main;
+
+/**
+ * This is the main where the program will be ran from
+ * 
+ * @author Christopher Baillie
+ * @version 1.0
+ * @since 1.0
+ */
+
+import java.awt.EventQueue;
+
+import view.MainMenu;
+
+public class Main {
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					//Centres the GUI to the middle of the screen
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
